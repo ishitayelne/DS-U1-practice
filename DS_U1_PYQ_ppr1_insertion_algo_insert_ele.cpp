6c671d7533fc8f29e66f793814091beb60ba55d4
Ishita Yelne <ishitayelne27@gmail.com>
2026-08-24 01:37:53 +0530

    Create DS_U1_PYQ_ppr1_insertion_algo_insert_ele.cpp

---
+++ DS_U1_PYQ_ppr1_insertion_algo_insert_ele.cpp
@@ -0,0 +1,20 @@
+//Paper 1
+//Q.1 B: Analyse the insertion algorithm of adding an element at any position in an array.
+#include <iostream>
+using namespace std;
+
+void insertAtPosition(int arr[], int &n, int capacity, int element, int pos) {
+    if (n >= capacity) {
+        cout << "Overflow: Array is full." << endl;
+        return;
+    }
+    if (pos < 0 || pos > n) {
+        cout << "Invalid position!" << endl;
+        return;
+    }
+    for (int i = n - 1; i >= pos; i--) {
+        arr[i + 1] = arr[i];
+    }
+    arr[pos] = element;
+    n++;
+}
