33bb0f0b88acb700be0797d5b7830074fda079c3
Ishita Yelne <ishitayelne27@gmail.com>
2026-08-24 07:13:09 +0530

    Update DS_U1_PYQ_ppr1_reverse_arr.cpp

--- DS_U1_PYQ_ppr1_reverse_arr.cpp
+++ DS_U1_PYQ_ppr1_reverse_arr.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 void reverseArray(int arr[], int size) {
-    int left = 0, right = size - 1;
-    while (left < right) {
-        int temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
-        left++;
-        right--;
+    int start = 0, end = size - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
     }
 }
 
@@ -20,6 +20,7 @@ int main() {
     // Get the size of the array from the user
     cout << "Enter the size of the array: ";
     cin >> size;
+
     int arr[size];
 
     // Get the array elements from the user
